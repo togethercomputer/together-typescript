5ac3a57c2508cce085a8f1d424914539cc61283e
pstern-sl <157847713+pstern-sl@users.noreply.github.com>
2024-05-17 15:19:48 -0400

    Updating fine-tune example (#8)

--- examples/fine-tune.ts
+++ examples/fine-tune.ts
@@ -1,31 +1,43 @@
 #!/usr/bin/env -S npm run tsn -T
 
-import  Together from 'together';
+import Together from 'together';
 
 const together = new Together();
 
 async function main() {
-  //Uploaded separately
+
+  //These files need to be uploaded separately. See the files example.
   const fileId = 'file-bf72b951-fa1a-41af-a152-fe385dca0201';
 
-  //Create a model
+  // Create a model
   const fineTuneModel = await together.fineTune.create({
-    model: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+    model: 'meta-llama/Meta-Llama-3-8B',
     training_file: fileId,
   });
 
   console.log(fineTuneModel);
 
-  // const ftId = 'ft-0b1d5942-d8f1-42ab-b0e9-af1528403740';
+  const fineTuneId = fineTuneModel.id;
+
+  //Wait for completion
+  while (true) {
+    const fineTuneStatus = await together.fineTune.retrieve(fineTuneId);
+    if (fineTuneStatus.status === 'completed') {
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
 
   //List the model events
-  const modelEvents = await together.fineTune.listEvents(fineTuneModel.id);
+  const modelEvents = await together.fineTune.listEvents(fineTuneId);
   for (const e of modelEvents.data) {
     console.log(e);
   }
 
+  //Download the model
   const down = await together.fineTune.download({
-    ft_id: fineTuneModel.id,
+    ft_id: fineTuneId,
   });
   console.log(JSON.stringify(down));
 }
