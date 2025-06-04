bb90a25b0433b454d19a77d45822618e46203878
David Meadows <dmeadows@stainless.com>
2025-06-04 10:22:27 -0400

    fix(tests): remove unused tests

--- tests/api-resources/files.test.ts
+++ tests/api-resources/files.test.ts
@@ -69,28 +69,4 @@ describe('resource files', () => {
       Together.NotFoundError,
     );
   });
-
-  test('upload: only required params', async () => {
-    const responsePromise = client.files.upload({
-      file: await toFile(Buffer.from('# my file contents'), 'README.md'),
-      file_name: 'dataset.csv',
-      purpose: 'fine-tune',
-    });
-    const rawResponse = await responsePromise.asResponse();
-    expect(rawResponse).toBeInstanceOf(Response);
-    const response = await responsePromise;
-    expect(response).not.toBeInstanceOf(Response);
-    const dataAndResponse = await responsePromise.withResponse();
-    expect(dataAndResponse.data).toBe(response);
-    expect(dataAndResponse.response).toBe(rawResponse);
-  });
-
-  test('upload: required and optional params', async () => {
-    const response = await client.files.upload({
-      file: await toFile(Buffer.from('# my file contents'), 'README.md'),
-      file_name: 'dataset.csv',
-      purpose: 'fine-tune',
-      file_type: 'jsonl',
-    });
-  });
 });
